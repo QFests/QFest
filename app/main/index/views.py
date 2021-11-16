07a397a8b1d9065d577949da62642020d0c49f25
Vitaliy Senko <senko.vit12@gmail.com>
2021-11-16 18:55:08 +0200

    Rebuild main page

--- app/main/index/views.py
+++ app/main/index/views.py
@@ -17,7 +17,7 @@ def index():
     the_alcohols = Alcohol.query
     if not current_user.can(Permission.UPDATE_ALCOHOL_INFORMATION):
         the_alcohols = the_alcohols.filter_by(hidden=0)
-    popular_alcohols = the_alcohols.outerjoin(Log).group_by(Alcohol.id).order_by(db.func.count(Log.id).desc()).limit(5)
+    popular_alcohols = the_alcohols.outerjoin(Log).group_by(Alcohol.id).order_by(db.func.count(Log.id).desc()).limit(12)
     popular_users = User.query.outerjoin(Log).group_by(User.id).order_by(db.func.count(Log.id).desc()).limit(5)
     recently_comments = Comment.query.filter_by(deleted=0).order_by(Comment.edit_timestamp.desc()).limit(5)
     return render_template("index.html", alcohols=popular_alcohols, users=popular_users, recently_comments=recently_comments,
